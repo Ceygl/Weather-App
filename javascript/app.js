43eac3d232f0aa015e4ba8bfbd271d87fefed80d
Ceygl <gulceyda98@gmail.com>
2022-09-28 13:50:53 -0400

    first commit

---
+++ javascript/app.js
@@ -0,0 +1,17 @@
+const city = document.querySelector("form input").textContent;
+console.log(city);
+const submit = document.querySelector("form button");
+
+
+submit.addEventListener("click", () => {
+
+})
+
+
+
+
+
+
+
+
+//!  API key: 6c7330a1f0c05fa244b561602395ec4d
